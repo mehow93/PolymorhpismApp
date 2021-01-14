6034a1c901e3a389535a600e304e104b76700d39
mehow93 <michmad93@gmail.com>
2021-01-14 11:23:40 +0100

    Added Reverse class

--- Operations.cpp
+++ Operations.cpp
@@ -1,3 +1,18 @@
 #include "Operations.h"
+#include <bits/stdc++.h>
 
+std::string DeleteChar::deleteChar(std::string& inputString, char charToDelete){
+    std::string result = inputString; //copy string to avoid overwritting orginal input
 
+    for (auto& stringChar : result){
+        if( stringChar == charToDelete)
+            stringChar = '_';
+    }
+    return result;
+}
+
+std::string ReverseData::reverseData(std::string& inputData){
+    std::string result = inputData;
+    reverse(result.begin(), result.end());
+    return result;
+};
