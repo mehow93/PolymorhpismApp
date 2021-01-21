76f39eb3017a0357fc252d72d5d71d600a2b4518
mehow93 <michmad93@gmail.com>
2021-01-21 10:46:51 +0100

    add getData()

---
+++ FileInput.cpp
@@ -0,0 +1,19 @@
+
+#include "FileInput.h"
+
+
+std::string FileInput::getData(std::string source){
+    std::ifstream fileOut;
+    fileOut.open(source);
+    std::string singleLine;
+    std::string result;
+    char c;
+    while(fileOut.get(c)){
+        result = result + c;
+    }
+
+    fileOut.close();
+    return result;
+
+
+}
