d669c01d9b97a3695087ca99904ec4230771b28d
mehow93 <michmad93@gmail.com>
2021-01-21 10:48:04 +0100

    refactoring

--- FileInput.cpp
+++ FileInput.cpp
@@ -5,15 +5,11 @@
 std::string FileInput::getData(std::string source){
     std::ifstream fileOut;
     fileOut.open(source);
-    std::string singleLine;
     std::string result;
     char c;
     while(fileOut.get(c)){
         result = result + c;
     }
-
     fileOut.close();
     return result;
-
-
 }
