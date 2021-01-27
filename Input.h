b4fc04c3697590e7d8f2b2d93cb79c337186bbd0
mehow93 <michmad93@gmail.com>
2021-01-27 07:51:47 +0100

    getData() argument deleted

--- Input.h
+++ Input.h
@@ -13,7 +13,7 @@ public:
     void setProcessedContent(std::string Input) {mProcessedContent = Input;}
     void setOriginalContent(std::string Input) {mOrginalContent = Input;}
     virtual ~Input() = default;
-    virtual std::string getData(std::string source) = 0;
+    virtual std::string getData() = 0;
 
 };
 #endif // BASEPARSER_H
