1c6d1cbfbcaca1cc75904bfaf368ea35fa559d63
mehow93 <michmad93@gmail.com>
2021-01-29 13:08:24 +0100

    TcpIpInput class added

---
+++ TcpIpInput.h
@@ -0,0 +1,15 @@
+#ifndef TCPIPINPUT_H
+#define TCPIPINPUT_H
+#include "Input.h"
+
+
+class TcpIpInput : public Input {
+
+
+public:
+    virtual ~TcpIpInput() = default;
+};
+
+
+
+#endif // TCPIPINPUT_H
